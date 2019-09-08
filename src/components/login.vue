a169dbfd6c97ca0aef98e7555852631470e389db
Guilherme Silva <guilhermesillva.grs@gmail.com>
2019-09-08 12:04:44 -0300

    #1 - telas temporarias login e singup

---
+++ src/components/login.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <h3>Entre</h3>
+        <input 
+            type='text' 
+            placeholder="Email" 
+            required>
+            <br>
+        <input 
+            type="password" 
+            placeholder="Senha" 
+            required>
+            <br>
+        <button>Entrar</button>
+        <p>Não possui conta? Crie uma! </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+            
+        },
+    }
+</script>
+
+<style scoped>
+input {
+    border-radius: 25px;
+    margin: 10px 0;
+    width: 20%;
+    cursor: pointer;
+    line-height: 30px;
+}
+button {
+    border-radius: 25px;
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+    
+}
+p {
+    margin-top: 40px;
+    font-size: 13px;
+}
+p a {
+    text-decoration: underline;
+    cursor: pointer;
+}
+
+</style>
---
+++ src/components/singUp.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <h3> Criar uma conta </h3>
+        <input 
+            type='text' 
+            placeholder='Email'
+            required>
+            <br>
+        <input 
+            type="password" 
+            placeholder="Senha"
+            required>
+            <br>
+        <button>Criar conta</button>
+        <p>Voltar para login</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'singUp',
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+           
+        },
+    }
+</script>
+
+<style scoped>
+input{
+    border-radius: 25px;
+    margin: 10px 0;
+    width: 20%;
+    cursor: pointer;
+    line-height: 30px;
+}
+button {
+    border-radius: 25px;
+    margin-top: 20px;
+    width: 10%;
+    cursor: pointer;
+}
+p {
+    margin-top: 40px;
+    font-size: 13px;
+}
+p a {
+    text-decoration: underline;
+    cursor: pointer;
+}
+
+</style>
