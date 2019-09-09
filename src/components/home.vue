480ab90c2e722de59d3757f7fc06f405b9fe436b
Guilherme Silva <guilhermesillva.grs@gmail.com>
2019-09-09 09:42:56 -0300

    #1 - implementando botao logout na tela home

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,17 +1,33 @@
 <template>
-  <div></div>
+  <div>
+    <h3>Bem Vindo!</h3>
+    <button @click='logout'>Sair</button>
+  </div>
 </template>
 
 <script>
+import firebase from 'firebase'
 export default {
   name: 'home',
   data () {
     return {}
   },
-  methods: {}
+  methods: {
+    logout: function() {
+      firebase.auth().signOut().then(() => {
+        this.$router.replace('/')
+      })
+    }
+  }
 }
 </script>
 
 <style scoped>
+button {
+  border-radius: 25px;
+  margin-top: 20px;
+  width: 10%;
+  cursor: pointer;
+}
 
 </style>
